b4b5e3a4a5b9861d17cb288436c6f2c9f5fd040a
zap <z@zap.cn>
2025-08-22 10:26:24 +0800

    update

--- zapd/src/main.rs
+++ zapd/src/main.rs
@@ -1,35 +1,33 @@
-use std::collections::HashMap;
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-low-level-rustls
+//! ```
+//! 
+//! 基本完成
+
 use std::time::Duration;
 
-use anyhow::Ok;
-use axum::Extension;
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse},
-    routing::{get,post},
-    Router,
+use axum::{extract::{Path, Request}, response::IntoResponse, routing::get, Extension, Router};
+use hyper::body::Incoming;
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use tokio::{io::AsyncWriteExt};
+use tokio_rustls::{
+    rustls::{pki_types::{pem::PemObject, CertificateDer, PrivateKeyDer}, ServerConfig},
+    TlsAcceptor,
 };
-use sqlx::{Executor, SqlitePool};
-use tokio::net::TcpListener;
-use tokio::signal;
-use tokio::time::sleep;
-use tower_http::timeout::TimeoutLayer;
-use tower_http::trace::{self, TraceLayer};
+use tower_http::trace::TraceLayer;
+use tower_service::Service;
+use tracing::{error, info, warn};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tokio::net::{TcpListener};
+use tower_http::timeout::TimeoutLayer;
 
-mod db;
 mod config;
-// use db::prepare_database;
-
-struct AppState {
-    jwt: HashMap<String, String>
-}
-
+mod db;
 
 #[tokio::main]
-async fn main()-> anyhow::Result<()> {
-    println!("{:?}",config::new());
-    // Enable tracing.
+async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -37,64 +35,102 @@ async fn main()-> anyhow::Result<()> {
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
-    // let pool = db::prepare_database().await?;
+
+    println!("{:?}",config::new());
+    let tls_acceptor = create_tls_acceptor("conf/zap.crt","conf/zap.key");
+    let bind = "0.0.0.0:2600";
+    let tcp_listener = TcpListener::bind(bind).await.unwrap();
+    info!("Zap server listening on {bind}. ");
     let conn = db::prepare_database().await.unwrap();
-    // Create a regular axum app.
-    let app = Router::new()
-        .route("/slow", get(|| sleep(Duration::from_secs(5))))
-        .route("/forever", get(std::future::pending::<()>))
-        .route("/hello",get(handler))
-        .layer((
-            TraceLayer::new_for_http(),
-            // Graceful shutdown will wait for outstanding requests to complete. Add a timeout so
-            // requests don't hang forever.
-            TimeoutLayer::new(Duration::from_secs(10)),
-            Extension(conn),
-        ));
-    let app = app.fallback(handler_404);
-    // Create a `TcpListener` using tokio.
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    // Run the server with graceful shutdown
-    axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap();
-    Ok(())
-}
+    let app = Router::new().route("/hello/{id}", get(hello)).route("/", get(handler)).layer((
+        TraceLayer::new_for_http(),
+        // Graceful shutdown will wait for outstanding requests to complete. Add a timeout so
+        // requests don't hang forever.
+        TimeoutLayer::new(Duration::from_secs(10)),
+        Extension(conn),
+    ));
+    
+    loop {
+        let tower_service = app.clone();
+        
 
-async fn handler_404() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "404 page")
-}
+        // Wait for new tcp connection
+        let (mut stream, addr) = tcp_listener.accept().await.unwrap();
+        let mut buf = [0; 1];
+        let n = stream.peek(&mut buf).await.unwrap();
 
-async fn handler(Extension(pool): Extension<SqlitePool>) {
-    // state.lock().unwrap().jwt.insert("k".to_string(), "v".to_string());
-    let _ = pool.execute("create table user (id int,name text)").await;
-}
+        if n > 0 && buf[0] == 0x16 {
+        
+            let tls_acceptor = tls_acceptor.clone();
+            tokio::spawn(async move {
+            // Wait for tls handshake to happen
+            let Ok(stream) = tls_acceptor.accept(stream).await else {
+                error!("error during tls handshake connection from {}", addr);
+                return;
+            };
 
+            // Hyper has its own `AsyncRead` and `AsyncWrite` traits and doesn't use tokio.
+            // `TokioIo` converts between them.
+            let stream = TokioIo::new(stream);
 
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
+            // Hyper also has its own `Service` trait and doesn't use tower. We can use
+            // `hyper::service::service_fn` to create a hyper `Service` that calls our app through
+            // `tower::Service::call`.
+            let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+                // We have to clone `tower_service` because hyper's `Service` uses `&self` whereas
+                // tower's `Service` requires `&mut self`.
+                //
+                // We don't need to call `poll_ready` since `Router` is always ready.
+                tower_service.clone().call(request)
+            });
 
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
+            let ret = hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
+                .serve_connection_with_upgrades(stream, hyper_service)
+                .await;
 
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
+            if let Err(err) = ret {
+                warn!("error serving connection from {}: {}", addr, err);
+            }
+            });
+        }else {
+            info!("request addr {}",addr);
+            let resp = format!(
+                "HTTP/1.1 301 Moved Permanently\r\nLocation: https://{host}:{port}/\r\nContent-Length: 0\r\n\r\n",
+                host = addr.ip(),
+                port = 2600
+            );
+            let _ = stream.write_all(resp.as_bytes()).await;
+            let _ = stream.flush().await;
+            continue;
+        }
+    
     }
 }
 
+async fn handler() -> &'static str {
+    "Hello, World!"
+}
+
+async fn hello(Path(id):Path<u32>) -> impl IntoResponse {
+    format!("id {}",id)
+}
+
+
+fn create_tls_acceptor(cert: &str,key :&str) -> TlsAcceptor {
+    let key = PrivateKeyDer::from_pem_file(key).unwrap();
+
+    let certs = CertificateDer::pem_file_iter(cert)
+        .unwrap()
+        .map(|cert| cert.unwrap())
+        .collect();
+
+    let mut config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .expect("bad certificate/key");
+
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+
+    TlsAcceptor::from(std::sync::Arc::new(config))
+}
 
